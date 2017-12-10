9421f898dfa3ddec45de438be280d7afa7f0bb94
pranay2492 <pranaysharma2492@gmail.com>
2017-12-10 19:19:20 +0530

    4 commit

--- bobApp/src/main/java/bobApp/App.java
+++ bobApp/src/main/java/bobApp/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("my first app - bobby sharma");
+System.out.println("my first app - bobby sharma pranay");
 	}
 
 }
