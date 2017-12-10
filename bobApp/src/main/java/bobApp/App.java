c8b391f72eb9b18f26b636902f533a3d3b436296
pranay2492 <pranaysharma2492@gmail.com>
2017-12-10 18:20:41 +0530

    BobApp

---
+++ bobApp/src/main/java/bobApp/App.java
@@ -0,0 +1,10 @@
+package bobApp;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my first app - bobby");
+	}
+
+}
---
+++ bobApp/src/test/java/bobApp/AppTest.java
@@ -0,0 +1,20 @@
+package bobApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		int i=10;
+	}
+	
+	@Test
+	public void testshow() {
+		int i=10;
+		System.out.print(i);
+	}
+	
+}
